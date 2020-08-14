391e9d70956a80d0c84d451d070e6f3e27163d89
Da0hn <vegancraft13@gmail.com>
2020-08-14 19:51:00 -0400

    Classe App refatorada

--- src/main/java/br/edu/ifmt/cba/gateway/app/App.java
+++ src/main/java/br/edu/ifmt/cba/gateway/app/App.java
@@ -12,18 +12,7 @@ public class App {
 
     public static void main(String... args) {
         try {
-            var port = -1;
-            // verificando se foi informado 1 argumento de linha de comando
-            if(args.length < 1) {
-                System.err.println("Uso: java tcp.server <port>");
-                System.exit(1);
-            }
-            // para garantir que somente inteiros serão atribuídos a porta
-            port = Integer.parseInt(args[0]);
-            if(port < 1024) {
-                System.err.println("A porta deve ser maior que 1024");
-                System.exit(1);
-            }
+            var port = processArgument(args);
             new GatewayCommunication(
                     new ReceivedDataService(DatabaseConnection.getEntityManager())
             ).listen(port);
@@ -32,6 +21,22 @@ public class App {
             System.err.println("Erro: " + e.toString());
             System.exit(1);
         }
+    }
+
+    private static Integer processArgument(String[] args) {
+        var port = -1;
+        // verificando se foi informado 1 argumento de linha de comando
+        if(args.length < 1) {
+            System.err.println("Uso: java tcp.server <port>");
+            System.exit(1);
+        }
+        // para garantir que somente inteiros serão atribuídos a porta
+        port = Integer.parseInt(args[0]);
+        if(port < 1024) {
+            System.err.println("A porta deve ser maior que 1024");
+            System.exit(1);
+        }
+        return port;
     }
 
 }
