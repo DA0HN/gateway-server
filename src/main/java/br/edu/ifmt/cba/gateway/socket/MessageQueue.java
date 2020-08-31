e1350d3230c7ad2d00c9f50628249c441b02c60e
Da0hn <vegancraft13@gmail.com>
2020-08-31 14:47:48 -0400

    Implementação do MessageQueue utilizando wait/notify

--- src/main/java/br/edu/ifmt/cba/gateway/socket/MessageQueue.java
+++ src/main/java/br/edu/ifmt/cba/gateway/socket/MessageQueue.java
@@ -1,8 +1,9 @@
 package br.edu.ifmt.cba.gateway.socket;
 
+import br.edu.ifmt.cba.gateway.utils.Logger;
+
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author daohn on 26/08/2020
@@ -10,41 +11,78 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class MessageQueue {
 
-    private final Queue<String> fila;
-    private final ReentrantLock lock;
+    private static final int CAPACITY = 10;
+
+    private final Queue<String> queue;
+    private boolean isEmpty = true;
 
     public MessageQueue() {
-        this.fila = new LinkedList<>();
-        this.lock = new ReentrantLock(false);
+        this.queue = new LinkedList<>();
     }
 
     public synchronized void enqueue(String msg) {
-        lock.lock();
         try {
-            fila.add(msg);
-        } finally {
-            lock.unlock();
+            while(queue.size() == CAPACITY) {
+                Logger.log(Thread.currentThread().getName() + ": Buffer is full," +
+                                   " waiting...");
+                wait();
+            }
+            Logger.log(Thread.currentThread().getName() + ": added " + msg
+                               + " into queue"
+            );
+            queue.add(msg);
+            // Signal consumer thread that, buffer has element now
+            Logger.log(Thread.currentThread().getName()
+                               + ": Signalling that buffer is no more empty now");
+            // Communicate waiting thread that queue is not empty now
+            isEmpty = false;
+            notifyAll();
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
-    public String getAndRemoveFirst() {
-        lock.lock();
-        try {
-            return fila.poll();
-        } finally {
-            lock.unlock();
+    public synchronized String dequeue() {
+        String ref;
+        while(isEmpty) {
+            try {
+                wait();
+            }
+            catch(InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        // Se a fila possuir apenas 1 pair aciona a flag
+        // para que os consumidores entrem em estado de espera
+        if(queue.size() == 1) {
+            isEmpty = true;
+        }
+        // Se a fila estiver vazia ou tiver terminado o processo
+        // retorna null
+        if(queue.size() == 0) {
+            return null;
+        }
+        // Se houver mais de 1 elemento na fila não há necessidade
+        // de acionar a flag isEmpty
+        Logger.log(Thread.currentThread().getName()
+                           + ": Queue size " + queue.size());
+        ref = queue.remove();
+        if(queue.size() == CAPACITY - 1) {
+            notifyAll();
+        }
+        return ref;
     }
 
     public synchronized int size() {
-        return fila.size();
-    }
-
-    public synchronized boolean isEmpty() {
-        return fila.isEmpty();
+        return queue.size();
     }
 
     public synchronized boolean isNotEmpty() {
         return !isEmpty();
+    }
+
+    public synchronized boolean isEmpty() {
+        return queue.isEmpty();
     }
 }
