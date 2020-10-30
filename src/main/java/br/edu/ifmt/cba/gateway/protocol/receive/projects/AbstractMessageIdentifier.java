375ae57dc10c0e5b9efb3538599a027bef8a8512
Gabriel <gabrielhondacba@gmail.com>
2020-10-30 11:42:05 -0400

    Corrigida identificação de mensagem com número com ponto flutuante

--- src/main/java/br/edu/ifmt/cba/gateway/protocol/receive/projects/AbstractMessageIdentifier.java
+++ src/main/java/br/edu/ifmt/cba/gateway/protocol/receive/projects/AbstractMessageIdentifier.java
@@ -3,17 +3,23 @@ package br.edu.ifmt.cba.gateway.protocol.receive.projects;
 import br.edu.ifmt.cba.gateway.protocol.receive.ProtocolException;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 /**
  * @author daohn on 20/08/2020
  * @project gateway_server
  */
 public abstract class AbstractMessageIdentifier {
+
+    private static final String regExp = "[\\x00-\\x20]*[+-]?(((((\\p{Digit}+)(\\.)?((\\p{Digit}+)?)([eE][+-]?(\\p{Digit}+))?)|(\\.((\\p{Digit}+))([eE][+-]?(\\p{Digit}+))?)|(((0[xX](\\p{XDigit}+)(\\.)?)|(0[xX](\\p{XDigit}+)?(\\.)(\\p{XDigit}+)))[pP][+-]?(\\p{Digit}+)))[fFdD]?))[\\x00-\\x20]*";
+    private static final Pattern pattern = Pattern.compile(regExp);
+
     /**
      * @param data conteúdo que será analisado
      * @throws ProtocolException caso a mensagem contenha alguma deformação gera uma exceção
      */
     public abstract void identify(String data) throws ProtocolException;
+
 
     /**
      * realiza uma análise dos dados constantes da mensagem. O MAC de quem enviou, o MAC de quem
@@ -44,7 +50,7 @@ public abstract class AbstractMessageIdentifier {
             // b8:27:eb:8e:94:f2 ! b8:27:eb:8e:94:f2 ! project ! msg !...
             var msg = Arrays.copyOfRange(parsedContent, 3, parsedContent.length);
             for(var m : msg) {
-                if(!m.chars().allMatch(Character::isLetterOrDigit)) {
+                if(!m.chars().allMatch((ch) -> Character.isLetterOrDigit(ch) || pattern.matcher(String.valueOf(ch)).matches())) {
                     throw new UnidentifiedMessageException(
                             "Mensagem possui caracteres inválidos: " + m + "\n");
                 }
